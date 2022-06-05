5f99b7a51fc71c5932d41e1ab7835238aabeb456
Adam Spanko <adam707@gmail.com>
2022-06-05 16:01:41 +0300

    still creating jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,7 +17,22 @@ const UserSessions = require('./../models/user_sessions').init(db, Sequelize);
       })
 }
 
+const verify_token = (req, res, next) => {
+
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.status(403).send("Access denied. {auth}");
+
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).send("Invalid token");
+    }
+};
+
+
 
 // module.exports = create_token
-module.exports = { create_token };
+module.exports = { create_token, verify_token };
 
